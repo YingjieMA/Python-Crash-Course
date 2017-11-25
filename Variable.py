9066efb59576bcb16e7bee430e9399e8782d6669
Yuki_MA <32018254+YingjieMA@users.noreply.github.com>
2017-11-25 23:58:57 +0800

    Create Variable.py

---
+++ Variable.py
@@ -0,0 +1,12 @@
+name = "Ada Lovelace"
+#upper & lower case letters
+print(name.upper())
+print(name.lower())
+#tab & enter
+print("Hello" + "\tPython" + "\n!")
+#square
+print(2**10)
+#Data type conversion
+age = 23
+print("Happy " + str(age) + "th Birthday")
+import this
